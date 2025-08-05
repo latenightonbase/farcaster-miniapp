989b17cc6dbd2e19de8e1a5819298170eab093c4
Risavdeb Patra <needleweed03@gmail.com>
2025-08-05 22:10:22 +0530

    all things same, cast button changed

--- utils/MiniKitProvider.tsx
+++ utils/MiniKitProvider.tsx
@@ -4,23 +4,26 @@ import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
 import { ReactNode } from "react";
 import { base } from "wagmi/chains";
 import Rainbow from "./rainbow";
+import { MiniAppProvider } from "@neynar/react";
 
 export function MiniKitContextProvider({ children }: { children: ReactNode }) {
   return (
-    <MiniKitProvider
-      apiKey={process.env.NEXT_PUBLIC_CDP_CLIENT_API_KEY}
-      chain={base}
-      notificationProxyUrl="/api/notification"
-      config={{
-        appearance: {
-          mode: "auto",
-          theme: "mini-app-theme",
-          name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-          logo: process.env.NEXT_PUBLIC_ICON_URL,
-        },
-      }}
-    >
-      <Rainbow>{children}</Rainbow>
-    </MiniKitProvider>
+    // <MiniAppProvider>
+      <MiniKitProvider
+        apiKey={process.env.NEXT_PUBLIC_CDP_CLIENT_API_KEY}
+        chain={base}
+        notificationProxyUrl="/api/notification"
+        config={{
+          appearance: {
+            mode: "auto",
+            theme: "mini-app-theme",
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            logo: process.env.NEXT_PUBLIC_ICON_URL,
+          },
+        }}
+      >
+        <Rainbow>{children}</Rainbow>
+      </MiniKitProvider>
+    // </MiniAppProvider>
   );
 }
