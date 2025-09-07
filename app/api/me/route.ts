2c10c56853066f7201c62c2cd8949a99f6b14bd3
Risavdeb Patra <needleweed03@gmail.com>
2025-09-07 11:09:25 +0530

    simplified auth logic

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -2,34 +2,9 @@ import { createClient } from "@farcaster/quick-auth";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const client = createClient();
-  const authorization = request.headers.get("Authorization");
 
-  console.log("Authorization header:", authorization);  
-
-  if (!authorization) {
-    return NextResponse.json({ status: 401, statusText: "Unauthorized" });
-  }
-  var jwt: string = "";
-  if (process.env.NEXT_PUBLIC_ENV === "DEV") {
-    console.log("Development mode: using DEV_HEADER");
-    jwt = process.env.DEV_HEADER as string;
-  }
-  else{
-jwt = authorization?.split(" ")[1] as string;
-  }
-  
-
-  console.log("JWT token:", jwt);
-
-  const payload = await client.verifyJwt({
-    token: jwt,
-    domain: process.env.HOSTNAME as string,
-  });
-
-  console.log("JWT payload:", payload);
-
-  const fidParam = payload.sub;
+  const url = new URL(request.url);
+  const fidParam = url.searchParams.get("fid");
   if (!fidParam) {
     return NextResponse.json(
       { error: "Missing fid parameter" },
